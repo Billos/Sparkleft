94df3007d1f773e7850c7eb35dfa2d60b25814bd
Brendan Goubin <brendan.goubin@free.fr>
2026-06-13 13:31:07 +0200

    feat: Add API endpoints to toggle hidden budgets and categories

---
+++ src/endpoints/hideCategory.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import pino from "pino"
+
+import { redis as connection, hiddenCategoriesKey } from "../redis"
+
+const logger = pino()
+
+export async function hideCategory(req: Request<{ categoryName: string }>, res: Response) {
+  const { categoryName } = req.params
+  logger.info("=================================== Hiding toggle category ===================================")
+  const hiddenCategories = await connection.lrange(hiddenCategoriesKey, 0, -1)
+  const isCategoryHidden = hiddenCategories.includes(categoryName)
+
+  if (isCategoryHidden) {
+    logger.info("Category with name %s is already hidden, removing from hidden categories", categoryName)
+    await connection.lrem(hiddenCategoriesKey, 0, categoryName)
+    return res.status(202)
+  } else {
+    logger.info("Category with name %s is not hidden, adding to hidden categories", categoryName)
+    await connection.rpush(hiddenCategoriesKey, categoryName)
+    return res.status(201)
+  }
+}
