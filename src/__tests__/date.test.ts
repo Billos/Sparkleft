ad9b58e4102eb35400c40feee56ab3959f69df41
Brendan Goubin <brendan.goubin@free.fr>
2026-06-28 19:10:41 +0200

    test: Add tests for dates

--- src/__tests__/date.test.ts
+++ src/__tests__/date.test.ts
@@ -1,20 +1,51 @@
-import { describe, expect, it } from "vitest"
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 
-import { getDateNow } from "../utils/date"
+import { getEndOfCurrentMonth, getStartOfCurrentMonth } from "../utils/date"
 
-describe("getDateNow", () => {
-  it("returns a valid DateTime object", () => {
-    const date = getDateNow()
-    expect(date.isValid).toBe(true)
+describe("getStartOfCurrentMonth", () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
-  it("returns a DateTime close to the current time", () => {
-    const before = Date.now()
-    const date = getDateNow()
-    const after = Date.now()
+  it("returns the first day for a mocked date in the first day of a month", () => {
+    vi.setSystemTime(new Date("2024-01-01T00:00:00Z"))
+    expect(getStartOfCurrentMonth()).toBe("2024-01-01")
+  })
 
-    const dateMs = date.toMillis()
-    expect(dateMs).toBeGreaterThanOrEqual(before)
-    expect(dateMs).toBeLessThanOrEqual(after)
+  it("returns the first day for a mocked date in the middle of a month", () => {
+    vi.setSystemTime(new Date("2024-03-15T12:34:56Z"))
+    expect(getStartOfCurrentMonth()).toBe("2024-03-01")
+  })
+
+  it("returns the first day for a mocked date on the last day of a month", () => {
+    vi.setSystemTime(new Date("2024-12-31T23:59:59Z"))
+    expect(getStartOfCurrentMonth()).toBe("2024-12-01")
+  })
+})
+
+describe("getEndOfCurrentMonth", () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it("returns the last day for a mocked date in the first day of a month", () => {
+    vi.setSystemTime(new Date("2024-02-01T00:00:00Z"))
+    expect(getEndOfCurrentMonth()).toBe("2024-02-29")
+  })
+
+  it("returns the last day for a mocked date in the middle of a month", () => {
+    vi.setSystemTime(new Date("2024-03-15T12:34:56Z"))
+    expect(getEndOfCurrentMonth()).toBe("2024-03-31")
+  })
+
+  it("returns the last day for a mocked date on the last day of a month", () => {
+    vi.setSystemTime(new Date("2024-12-31T23:59:59Z"))
+    expect(getEndOfCurrentMonth()).toBe("2024-12-31")
   })
 })
